4e48db20493cf0bfe802d509be72fd5e9ca0b3da
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-18 18:59:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function createPost() {
   let postBody = document.getElementById("postBody").value
   let postAuthor = document.getElementById("postAuthor").value
 
-  
+  let postTemplate = document.getElementById("post-template").innerHTML
+  let pageTemplate = document.getElementById("page-template").innerHTML
 
 }
